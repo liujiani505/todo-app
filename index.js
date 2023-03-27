9b5326346c7ea4db6eee68c14b0bd05179ac9576
jianiliu <liujiani505@gmail.com>
2023-03-27 15:41:53 -0400

    main js code

---
+++ index.js
@@ -0,0 +1,5 @@
+const taskInput = document.querySelector(".task-input input"),
+    filters = document.querySelectorAll(".filters span"),
+    clearAll = document.querySelector(".clear-btn"),
+    taskBox = document.querySelector(".task-box");
+
